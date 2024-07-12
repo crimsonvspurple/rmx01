1b066c0d17a37abf4af20c8c44d6ca3ef0b66289
crimsonvspurple <11375716+crimsonvspurple@users.noreply.github.com>
2024-07-12 10:59:57 +0200

    tested a proof of concept that we can actually do the dom compare as we want
    Signed-off-by: crimsonvspurple <11375716+crimsonvspurple@users.noreply.github.com>

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,20 +1,24 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import {
-  Form,
-  isRouteErrorResponse,
-  useLoaderData,
-  useRouteError,
-} from "@remix-run/react";
+import { Form, isRouteErrorResponse, useLoaderData, useRouteError } from "@remix-run/react";
+import { compare , GroupingReporter } from "dom-compare";
+import { JSDOM } from 'jsdom';
 import invariant from "tiny-invariant";
 
 import { deleteNote, getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 
+// const { reporter } = require("dom-compare/lib/reporters");
+
+
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
   invariant(params.noteId, "noteId not found");
 
+  // console.log("COMPARE: ");
+  // const left = new JSDOM("<div class=\"note-html\">Hello, World!</div>").window.document.body;
+  // const right = new JSDOM("<div class=\"note-html2\">Hello, World!</div>").window.document.body;
+  // console.log(GroupingReporter.report(compare(left,right)));
   const note = await getNote({ id: params.noteId, userId });
   if (!note) {
     throw new Response("Not Found", { status: 404 });
@@ -37,7 +41,8 @@ export default function NoteDetailsPage() {
   return (
     <div>
       <h3 className="text-2xl font-bold">{data.note.title}</h3>
-      <p className="py-6">{data.note.body}</p>
+      {/*<p className="py-6">{data.note.body}</p>*/}
+      <div className="test-html" dangerouslySetInnerHTML={{ __html: data.note.body }} />
       <hr className="my-4" />
       <Form method="post">
         <button
